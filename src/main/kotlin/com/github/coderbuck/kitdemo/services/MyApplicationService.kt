47d03439808dcf706e07b12e00aba9334ab706a6
GitHub Action <action@github.com>
2021-06-24 07:53:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/coderbuck/kitdemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/coderbuck/kitdemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.coderbuck.kitdemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.coderbuck.kitdemo.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/coderbuck/kitdemo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/coderbuck/kitdemo/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.coderbuck.kitdemo.services
 
+import com.github.coderbuck.kitdemo.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
